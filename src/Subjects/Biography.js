4b57466122a8918841000385e49c539ae922a1f3
kelvinkyendwa <kelvinkyendwa@gmail.com>
2018-02-12 15:12:33 +0300

    New React project

---
+++ src/Subjects/Biography.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Biography extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Biography Tips</h2>
+        <p>
+        Reading might help
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Biography;
